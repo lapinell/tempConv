1b2ee12182611b35cea481e61783dd3c7d870cd5
lapinell <lapinell@gmail.com>
2018-02-01 14:41:21 -0600

    added converter formulas

--- converter.js
+++ converter.js
@@ -3,14 +3,23 @@
 // If the temperature is less than 32F/0C the color of the converted temperature should be blue.
 // For any other temperature, the color should be green.
 
+fahrenheitTemp;
+celsiusTemp;
 
-function toCelsius () {
-
+function toCelsius (Tfah) {
+    // Tcel = (Tfah - 32) * 1.8
+    let tCel = (Tfah - 32) * 1.8;
+    return tCel;
 }
 
-function toFahrenheit () {
-
+function toFahrenheit (Tcel) {
+    // Tfah = (Tcel/1.8) + 32
+    let tFah = (Tcel/1.8) + 32;
+    return tFah;
 }
+
+console.log(toCelsius(fahrenheitTemp));
+console.log(toFahrenheit(celsiusTemp));
 
 // Get a reference to the button element in the DOM
 var button = document.getElementById("converter");
