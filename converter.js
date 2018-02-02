cd1e4592b2f4918aeae2fa5e688187d70d88c623
lapinell <lapinell@gmail.com>
2018-02-01 23:37:20 -0600

    still building

--- converter.js
+++ converter.js
@@ -1,39 +1,6 @@
 const temp = document.getElementById("temp").value;
-const fahrenheitScale = document.getElementById("fahrenheit").checked;
-const celsiusScale = document.getElementById("celsius").checked;
 
 console.log("temp", temp);
-console.log("degF", fahrenheitScale);
-console.log("degC", celsiusScale);
-
-switch (true) {
-    case fahrenheitScale === true: 
-        console.log("fahrenheit works");
-        function toCelsius (temp) {
-            // Tcel = (Tfah - 32) * 1.8
-            let tCel = (temp - 32) * 1.8;
-            return tCel;
-        };
-        break;
-    case celsiusScale === true:
-        console.log("celsius works");
-        function toFahrenheit (temp) {
-            // Tfah = (Tcel/1.8) + 32
-            let tFah = (temp/1.8) + 32;
-            console.log(tFah);
-        }
-        break;
-    default:
-        console.log("This ain't workin right");
-}
-
-
-
-
-
-// Get a reference to the button element in the DOM
-// var button = document.getElementById("converter");
-
 
 //if the radio button for Celsius is chosen
 //take the number stored in celsiusTemp and convert it to Fahrenheit 
@@ -41,11 +8,55 @@ switch (true) {
 //if the radio button for Fahrenheit is chosen
 // // take the number stored in fahrenheitTemp and convert it to Celsius
 
+const fahrenheitScale = document.getElementById("fahrenheit").checked;
+const celsiusScale = document.getElementById("celsius").checked;
 
+console.log("degF", fahrenheitScale);
+console.log("degC", celsiusScale);
 
+function convertTemp(temp) {
+    switch (true) {
+        case fahrenheitScale === true: 
+            console.log("fahrenheit works");
+            function toCelsius (temp) {
+                // Tcel = (Tfah - 32) * 1.8
+                let tCel = (temp - 32) * 1.8;
+                return tCel;
+            };
+            break;
+        case celsiusScale === true:
+            console.log("celsius works");
+            function toFahrenheit (temp) {
+                // Tfah = (Tcel/1.8) + 32
+                let tFah = (temp/1.8) + 32;
+                console.log(tFah);
+            }
+            break;
+        default:
+            console.log("This ain't workin right. Try again.");
+    }
+}
+
+// Get a reference to the button element in the DOM
+// var button = document.getElementById("converter");
+
+const submitButton = document.getElementById("converter");
 
 // Assign a function to be executed when the button is clicked
 // button.addEventListener( determineConverter);
+
+
+
+let clearForm = () => {
+    document.getElementById("tempConverter").reset();
+}
+
+
+
+
+const resetButton = document.getElementById("resetForm");
+
+resetButton.addEventListener("reset", clearForm());
 
 // Add an event handler to the input field that checks if the user pressed the enter key, and if that happens, perform the conversion.
 
