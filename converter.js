57f02ee1792b21c8a16c59cb00f2e7aaca02cddd
lapinell <lapinell@gmail.com>
2018-02-02 10:44:37 -0600

    fixed scoping issues and got calculator to work

--- converter.js
+++ converter.js
@@ -1,4 +1,4 @@
-const temp = document.getElementById("temp").value;
+const temp = document.getElementById("temp");
 
 console.log("temp", temp);
 
@@ -8,34 +8,41 @@ console.log("temp", temp);
 //if the radio button for Fahrenheit is chosen
 // // take the number stored in fahrenheitTemp and convert it to Celsius
 
-const fahrenheitScale = document.getElementById("fahrenheit").checked;
-const celsiusScale = document.getElementById("celsius").checked;
 
-console.log("degF", fahrenheitScale);
-console.log("degC", celsiusScale);
+// console.log("degF", fahrenheitScale);
+// console.log("degC", celsiusScale);
 
-let convertedTemp = (t) => {
-                        switch (true) {
-                            case fahrenheitScale === true: 
-                                console.log("fahrenheit works");
-                                function toCelsius (temp) {
-                                    // Tcel = (Tfah - 32) * 1.8
-                                    let tCel = (temp - 32) * 1.8;
-                                    return tCel;
-                                };
-                                break;
-                            case celsiusScale === true:
-                                console.log("celsius works");
-                                function toFahrenheit (temp) {
-                                    // Tfah = (Tcel/1.8) + 32
-                                    let tFah = (temp/1.8) + 32;
-                                    return tFah;
-                                }
-                                break;
-                            default:
-                                console.log("This ain't workin right. Try again.");
-                        }
-                    };
+let convertedTemp = () => {
+
+    const fahrenheitScale = document.getElementById("fahrenheit").checked;
+
+    const celsiusScale = document.getElementById("celsius").checked;
+
+
+    switch (true) {
+        case fahrenheitScale: 
+            console.log("fahrenheit works");
+            function toCelsius (tF) {
+                // Tcel = (Tfah - 32) / 1.8
+                let tCel = (tF - 32) / 1.8;
+                return tCel;
+            };
+            console.log(toCelsius(temp.value));
+            break;
+        case celsiusScale:
+            console.log("celsius works");
+            function toFahrenheit (tC) {
+                // Tfah = (Tcel/1.8) + 32
+                let tFah = tC * 1.8 + 32;
+                return tFah;
+            }
+            console.log(toFahrenheit(temp.value));
+            break;
+        default:
+            console.log("This ain't workin right. Try again.");
+    }
+};
+
 
 // Get a reference to the button element in the DOM
 // var button = document.getElementById("converter");
@@ -45,25 +52,20 @@ const submitButton = document.getElementById("converter");
 // Assign a function to be executed when the button is clicked
 // button.addEventListener( determineConverter);
 
-let submitForm = () => {
-    submitButton.addEventListener("click", console.log(convertedTemp));
-}
+submitButton.addEventListener("click", convertedTemp);
+
+
+
+// let clearForm = () => {
+//     document.getElementById("tempConverter").reset();
+// }
 
 
 
 
+// const resetButton = document.getElementById("resetForm");
 
-
-let clearForm = () => {
-    document.getElementById("tempConverter").reset();
-}
-
-
-
-
-const resetButton = document.getElementById("resetForm");
-
-resetButton.addEventListener("reset", clearForm());
+// resetButton.addEventListener("reset", clearForm());
 
 // Add an event handler to the input field that checks if the user pressed the enter key, and if that happens, perform the conversion.
 
