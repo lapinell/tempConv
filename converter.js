bdc31ca13f05a0123f87e737414d72498007074a
lapinell <lapinell@gmail.com>
2018-02-02 22:54:46 -0600

    added color and reworked parts

--- converter.js
+++ converter.js
@@ -1,5 +1,4 @@
 const temp = document.getElementById("temp");
-let printHere = document.getElementById("displayTemp");
 
 //if the radio button for Celsius is chosen
 //take the number stored in celsiusTemp and convert it to Fahrenheit 
@@ -7,17 +6,7 @@ let printHere = document.getElementById("displayTemp");
 //if the radio button for Fahrenheit is chosen
 // // take the number stored in fahrenheitTemp and convert it to Celsius
 
-function toCelsius () {
-    // Tcel = (Tfah - 32) / 1.8
-    let tCel = (temp.value - 32) / 1.8;
-    return tCel;
-};
 
-function toFahrenheit () {
-    // Tfah = (Tcel/1.8) + 32
-    let tFah = temp.value * 1.8 + 32;
-    return tFah;
-}
 
 // console.log("degF", fahrenheitScale);
 // console.log("degC", celsiusScale);
@@ -28,40 +17,50 @@ let convertedTemp = () => {
 
     const celsiusScale = document.getElementById("celsius").checked;
 
+    let printHere = document.getElementById("displayTemp");
+
+    function toCelsius () {
+        // Tcel = (Tfah - 32) / 1.8
+        let tCel = (temp.value - 32) / 1.8;
+        return tCel;
+    };
+    
+    function toFahrenheit () {
+        // Tfah = (Tcel/1.8) + 32
+        let tFah = temp.value * 1.8 + 32;
+        return tFah;
+    }
+
     switch (true) {
         case fahrenheitScale: 
             console.log("fahrenheit works");
-            printHere.innerText = toCelsius().toString() + " degrees Celsius";
+            printHere.innerText = Math.floor(toCelsius()).toString() + " degrees Celsius";
             break;
         case celsiusScale:
             console.log("celsius works");
-            printHere.innerText = toFahrenheit().toString() + " degress Fahrenheit";
+            printHere.innerText = Math.floor(toFahrenheit()).toString() + " degress Fahrenheit";
             break;
         default:
             console.log("This ain't workin right. Try again.");
     }
-};
-
 // If the temperature is greater than 90F/32C the color of the converted temperature should be red.
 // If the temperature is less than 32F/0C the color of the converted temperature should be blue.
 // For any other temperature, the color should be green.
-
-let tempColor = () => {
     switch (true) {
-                   case toFahrenheit() > 90 || toCelsius() > 32:
-                       console.log(temp.value);
-                       console.log("red");
-                       break;
-                   case toFahrenheit() < 32 || toCelsius() < 0:
-                       console.log(temp.value)
-                       console.log("blue");
-                       break;
-                   default:
-                       console.log("green");
-               }
+                    case toFahrenheit() > 90 || toCelsius() > 32:
+                        console.log("to Celsius", toCelsius(), "temp input", temp.value, "red");
+                        printHere.style.color = "red";
+                        break;
+                    case toFahrenheit() < 32 || toCelsius() < 0:
+                        console.log("toFah", toFahrenheit(), "temp input", temp.value, "blue");
+                        printHere.style.color = "blue";
+                        break;
+                    default:
+                        console.log("temp input", temp.value, "green");
+                        printHere.style.color = "green";
+                }
 
-};
-
+    };
 
 // Get a reference to the button element in the DOM
 // var button = document.getElementById("converter");
@@ -69,11 +68,8 @@ let tempColor = () => {
 const submitButton = document.getElementById("converter");
 
 // Assign a function to be executed when the button is clicked
-// button.addEventListener( determineConverter);
 
 submitButton.addEventListener("click", convertedTemp);
-submitButton.addEventListener("click", tempColor);
-
 
 const resetButton = document.getElementById("resetForm");
 
@@ -91,8 +87,7 @@ let enterKeyStroke = () => {
         event.preventDefault();
         console.log("you've pressed enter");
         convertedTemp();
-        tempColor();
-    }
+s    }
 };
 
 
